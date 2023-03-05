a2da210d3c32372d5f53846ef1cab3fffc86af3e
SebastianMunaretto <sebastianmunaretto@gmail.com>
2023-03-05 10:10:24 +0100

    Finished user register with email and password

--- AudioAIClient/src/app/app.module.ts
+++ AudioAIClient/src/app/app.module.ts
@@ -1,24 +1,32 @@
+import { OpenAiConnectionService } from 'src/app/services/open-ai-connection.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavigationContainerComponent } from './components/navigation-container/navigation-container.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
 import { SettingsComponent } from './components/settings/settings.component';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatDialogModule } from '@angular/material/dialog';
 import { AudioRecorderPopupComponent } from './components/audio-recorder-popup/audio-recorder-popup.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { RegisterComponent } from './components/register/register.component';
+import { environment } from '../environments/environment';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+
 
 @NgModule({
   declarations: [
@@ -34,7 +42,10 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatInputModule,
     LayoutModule,
+    MatFormFieldModule,
     HttpClientModule,
     MatToolbarModule,
     MatButtonModule,
@@ -42,9 +53,12 @@ import { HttpClientModule } from '@angular/common/http';
     MatIconModule,
     MatListModule,
     MatDialogModule,
-    MatTooltipModule
+    MatTooltipModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
   ],
-  providers: [],
+  providers: [OpenAiConnectionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- AudioAIClient/src/app/components/register/register.component.ts
+++ AudioAIClient/src/app/components/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import {Auth, sendPasswordResetEmail, signOut, createUserWithEmailAndPassword} from '@angular/fire/auth';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+
+  registerForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)])
+  });
+
+  constructor(private auth: Auth, private router: Router) { }
+
+  async registerWithEmail() {
+    const { email, password } = this.registerForm.value;
+    try {
+      const credential = await createUserWithEmailAndPassword(this.auth , email!, password!);
+      this.router.navigate(['/']);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
 }
